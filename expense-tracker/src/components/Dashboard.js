134fde08e58240ede72a3fc9749ce01f769fb540
Shubham Chauhan <sc8040197@gmail.com>
2025-02-07 22:55:56 +0530

    dashboard ui change

--- expense-tracker/src/components/Dashboard.js
+++ expense-tracker/src/components/Dashboard.js
@@ -1,37 +1,47 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import img1 from '../asset/img/dashboard.jpg'
-import { Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 function Dashboard() {
-
- const navigate = useNavigate();
- const [inputs, setinputs] = useState({})
- const [showerror,setshowerror]=useState(false)
- const handlechange =(event)=>{
-const name = event.target.name;
-const value = event.target.value;
- setinputs(values =>({...values,[name]:value}))
-
- }
+  const myexpenses = [100,200,300,600,700,800]
 
 
-  const dologin = (event)=>{
-    event.preventDefault();
-console.log("Clicked", inputs);
-
-if(inputs.pass.length>=8){
-  navigate("/dashboard");
-}
-else{
-  setshowerror(true);
-}
+  const navigate = useNavigate();
+  const [inputs, setinputs] = useState({})
+  const [showdata, setshowdata] = useState(false)
+  const handlechange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setinputs(values => ({ ...values, [name]: value }))
 
   }
+
+
+  const addexpense = (event) => {
+    event.preventDefault();
+    console.log("Clicked", inputs);
+    setshowdata(true)
+  }
+
   return (
     <div className='container'>
+      {showdata ?
+        <div className='leftcontainer'>
+          <p>Total expenses : 1000</p>
+          {myexpenses.map((val) => 
+          <div className='expensecard'>
+            <p>Title</p>
+            <p>Amount {val}</p>
+            <p>Date</p>
+            <p>Type</p>
+             </div>
+            )}
+          </div>
+          :
       <div className='leftcontainer'>
         <p>please add your expenses</p>
         <img src={img1} />
       </div>
+}
 
 
       <div className='rightcontainer'>
@@ -39,50 +49,68 @@ else{
           <div>
             <h2 style={{ alignSelf: "center" }}>Add Expense</h2></div>
 
-            <form onsubmit={dologin}>
-          <div>
-            <label>E-mail</label>
-            <input
-              required
-              type="e-mail"
-              placeholder='Enter your E-mail'
-              value={inputs.email||''}
-              onChange={handlechange}
-              name='email'
-               />
-         
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              required
-              type="password"
-              placeholder='Enter your Password'
-              value={inputs.pass||''}
-              onChange={handlechange}
-              name='pass'
-               />
-             
-          </div>
-          {
-            showerror ? 
-          <div>
-           <span style={{color:'red',alignSelf:'center'}}>Password length must be greater then 8</span>
-            </div> : null
-}
+          <form onSubmit={addexpense}>
+            <div>
+              <label>Title</label>
+              <input
+                required
+                type="text"
+                placeholder='Enter your expense title'
+                value={inputs.title || ''}
+                onChange={handlechange}
+                name='title'
+              />
 
-          <div>
-            <button>Add</button>
             </div>
             <div>
-                <span style={{ alignSelf: 'center' }}>not a user ?<Link to="/register">Register</Link> </span>
-              </div>
-              </form>
-          </div>
-        </div>
+              <label>Amount</label>
+              <input
+                required
+                type="number"
+                placeholder='Enter your expense amount'
+                value={inputs.expense || ''}
+                onChange={handlechange}
+                name='expense'
+              />
 
+            </div>
+            <div>
+              <label>select type</label>
+              <select
+                value={inputs.type||''}
+                onChange={handlechange}
+                name='type'
+              >
+                <option value="cash">Cash</option>
+                <option value="upi">UPI</option>
+                <option value="card">Card</option>
+              </select>
+
+            </div>
+            <div>
+              <label>date</label>
+              <input
+                required
+                type="date"
+                placeholder='Enter date'
+                value={inputs.date || ''}
+                onChange={handlechange}
+                name='date'
+              />
+
+            </div>
+
+
+            <div>
+              <button>Add</button>
+            </div>
+
+          </form>
+        </div>
       </div>
-      )
+
+    </div>
+  )
 }
 
-      export default Dashboard
+export default Dashboard
